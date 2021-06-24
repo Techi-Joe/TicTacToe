2135924dee9ac02b8cb295509cec2e89c78ae0e4
Techi-Joe <techi.jo3@gmail.com>
2021-06-23 21:23:56 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company.games.Games;
+
+public class Main {
+
+    public static void main(String[] args) {
+        board b = new board();
+        b.place(true, 1);
+        System.out.println(b);
+    }
+}
---
+++ board.java
@@ -0,0 +1,31 @@
+package com.company.games.Games;
+
+import java.util.Arrays;
+
+public class board {
+    private final String[] board = new String[6];
+
+    public board() {
+        // clear board
+        for (int i = 0; i < 6; i++) {
+            board[i] = "";
+        }
+    }
+
+    public String[] getBoard() {
+        return board;
+    }
+
+    public void place(boolean p, int x) {
+        if (p) {
+            board[x] = "X";
+        } else {
+            board[x] = "O";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(board);
+    }
+}
